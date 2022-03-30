0cb5de9aaded1cfd0d51451f4073e1c1891f78b2
LLT <wolilitian@gmail.com>
2022-03-30 10:56:43 +0200

    small change

--- data_extraction.py
+++ data_extraction.py
@@ -94,7 +94,7 @@ def create_data_frames_highway(x):
 
         list_df = np.array_split(tempdata, 5)
         for j in range(0, len(list_df)):
-            data.append((list_df[j], i))
+            data.append((list_df[j], i-1, 'highway'))
 
     return data
 
@@ -126,7 +126,7 @@ def create_data_frame_suburban(x):
 
         list_df = np.array_split(tempdata, 5)
         for j in range(0, len(list_df)):
-            data.append((list_df[j], i))
+            data.append((list_df[j], i-1, 'suburban'))
 
 
     return data
@@ -275,12 +275,12 @@ def create_data_frame_tutorial(x):
             list_df_new.append(last)
 
             for j in range(0, len(list_df_new)):
-                data.append((list_df_new[j], i))
+                data.append((list_df_new[j], i-1, 'tutorial'))
 
         else:
             list_df = np.array_split(tempdata, 5)
             for j in range(0, len(list_df)):
-                data.append((list_df[j], i))
+                data.append((list_df[j], i-1, 'tutorial'))
 
     return data
 
@@ -312,6 +312,6 @@ def create_data_frame_urban(x):
 
         list_df = np.array_split(tempdata, 5)
         for j in range(0, len(list_df)):
-            data.append((list_df[j], i))
+            data.append((list_df[j], i-1, 'urban'))
 
     return data
